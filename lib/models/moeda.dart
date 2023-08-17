c93de8d40ed117e77937f074c42111ea6a263c8f
doni-zete <donizete112@gmail.com>
2023-08-17 00:33:04 -0300

    feat: modal moeda

--- lib/models/moeda.dart
+++ lib/models/moeda.dart
@@ -2,16 +2,22 @@ import 'package:json_annotation/json_annotation.dart';
 
 @JsonSerializable()
 class Moeda {
-  Moeda({required this.title, required this.dateTime});
+  Moeda({
+    required this.title,
+    required this.valor, // Adicionar o campo 'valor'
+    required this.dateTime,
+  });
 
   Moeda.fromJson(Map<String, dynamic> json)
       : title = json['title'],
+        valor = json['valor'] != null ? json['valor'].toDouble() : 0.0,
         dateTime = DateTime.parse(json['datetime']);
 
   String title;
+  double valor; // Adicionar o campo 'valor'
   DateTime dateTime;
 
   Map<String, dynamic> moedaJson() {
-    return {'title': title, 'datetime': dateTime.toIso8601String()};
+    return {'title': title, 'valor': valor, 'datetime': dateTime.toIso8601String()};
   }
 }
