45ecfa2ea9d09d04bc277adad237b23f18780678
doni-zete <donizete112@gmail.com>
2023-09-24 17:49:15 -0300

    feat: adicionando img da api coinmarketcap

--- lib/api/coin_api.dart
+++ lib/api/coin_api.dart
@@ -5,7 +5,7 @@ const String apiKey = 'de4cd560-617e-4737-8a3a-487d0a668c3d';
 const String requestBaseUrl =
     "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
 
-Future<Map<dynamic, dynamic>?> getData(String coinId) async {
+Future<Map<String, dynamic>?> getData(String coinId) async {
   String request =
       "$requestBaseUrl?symbol=$coinId&convert=USD&CMC_PRO_API_KEY=$apiKey";
 
@@ -13,7 +13,7 @@ Future<Map<dynamic, dynamic>?> getData(String coinId) async {
 
   if (response.statusCode == 200) {
     // Decodificar o JSON da resposta
-    Map<dynamic, dynamic> data = json.decode(response.body);
+    Map<String, dynamic> data = json.decode(response.body);
 
     // Verificar se a resposta contém os dados esperados
     if (data.containsKey('data') &&
@@ -24,14 +24,31 @@ Future<Map<dynamic, dynamic>?> getData(String coinId) async {
         data['data'][coinId]['quote'] is Map<String, dynamic> &&
         data['data'][coinId]['quote']['USD'] is Map<String, dynamic> &&
         data['data'][coinId]['quote']['USD'].containsKey('price')) {
-      // Retorna os dados obtidos
       return data;
-    } else {
-      print('Dados ausentes ou em formato inválido na resposta da API.');
-      return null;
     }
-  } else {
-    print('Falha ao carregar os dados: ${response.statusCode}');
-    return null;
   }
+
+  return null;
+}
+
+Future<String?> getCoinImage(String coinId) async {
+  String imageRequest =
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol=$coinId&CMC_PRO_API_KEY=$apiKey";
+
+  http.Response imageResponse = await http.get(Uri.parse(imageRequest));
+
+  if (imageResponse.statusCode == 200) {
+    Map<String, dynamic> imageData = json.decode(imageResponse.body);
+
+    if (imageData.containsKey('data') &&
+        imageData['data'] is Map<String, dynamic> &&
+        imageData['data'].containsKey(coinId) &&
+        imageData['data'][coinId] is Map<String, dynamic> &&
+        imageData['data'][coinId].containsKey('logo') &&
+        imageData['data'][coinId]['logo'] is String) {
+      return imageData['data'][coinId]['logo'];
+    }
+  }
+
+  return null;
 }
