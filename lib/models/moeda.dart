6bd7c57c7bdae8c66e7b084b4955c3ae25cbc749
doni-zete <donizete112@gmail.com>
2023-09-24 17:39:44 -0300

    feat: alterando modal add id user  para autenticar

--- lib/models/moeda.dart
+++ lib/models/moeda.dart
@@ -3,21 +3,29 @@ import 'package:json_annotation/json_annotation.dart';
 @JsonSerializable()
 class Moeda {
   Moeda({
+    required this.id, // Adicionar o campo 'id'
     required this.title,
-    required this.valor, // Adicionar o campo 'valor'
+    required this.valor,
     required this.dateTime,
   });
 
   Moeda.fromJson(Map<String, dynamic> json)
-      : title = json['title'],
+      : id = json['id'], 
+        title = json['title'],
         valor = json['valor'] != null ? json['valor'].toDouble() : 0.0,
         dateTime = DateTime.parse(json['datetime']);
 
+  String id; 
   String title;
-  double valor; // Adicionar o campo 'valor'
+  double valor;
   DateTime dateTime;
 
   Map<String, dynamic> moedaJson() {
-    return {'title': title, 'valor': valor, 'datetime': dateTime.toIso8601String()};
+    return {
+      'id': id, 
+      'title': title,
+      'valor': valor,
+      'datetime': dateTime.toIso8601String()
+    };
   }
 }
