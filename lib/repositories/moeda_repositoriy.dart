1668798e51d837edc2f9562e4dbe9820b2fcf7df
doni-zete <donizete112@gmail.com>
2023-09-24 18:15:25 -0300

    fix: add moeda ao usuario do firestore, removendo do shared_preference

--- lib/repositories/moeda_repositoriy.dart
+++ lib/repositories/moeda_repositoriy.dart
@@ -1,26 +1,43 @@
-import 'dart:convert';
-import "package:cripto_github/models/moeda.dart";
-import 'package:shared_preferences/shared_preferences.dart';
-
-const moedaListKey = 'moeda_list';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cripto_github/models/moeda.dart';
 
 class MoedaRepository {
-  late SharedPreferences sharedPreferences;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-// CARREGANDO A LISTA MOEDA
-  Future<List<Moeda>> getMoedaList() async {
-    sharedPreferences = await SharedPreferences.getInstance();
-    final String jsonString = sharedPreferences.getString(moedaListKey) ?? '[]';
-    final List jsonDecoded = json.decode(jsonString) as List;
-    return jsonDecoded.map((e) => Moeda.fromJson(e)).toList();
+  // CARREGANDO A LISTA DE MOEDAS DE UM USUÁRIO
+
+  Future<List<Moeda>> getMoedaList(String userId) async {
+    QuerySnapshot querySnapshot = await firestore
+        .collection('usuarios')
+        .doc(userId)
+        .collection('moedas')
+        .get();
+
+    List<Moeda> moedas = [];
+
+    querySnapshot.docs.forEach((document) {
+      moedas.add(Moeda.fromJson(document.data() as Map<String, dynamic>));
+    });
+
+    return moedas;
   }
 
-// SALVANDO A MOEDA EM UMA LISTA;
-  void saveMoedaList(List<Moeda> listaMoeda) {
-    final List<Map<String, dynamic>> jsonList =
-        listaMoeda.map((todo) => todo.moedaJson()).toList();
-    final String jsonString = json.encode(jsonList);
-    sharedPreferences.setString('moeda_list', jsonString);
-    print(jsonString);
+  // SALVANDO A MOEDA EM UMA LISTA PARA UM USUÁRIO
+  // No MoedaRepository
+  Future<void> saveMoedaList(String userId, List<Moeda> listaMoeda) async {
+    CollectionReference userCollection = firestore.collection('usuarios');
+    DocumentReference userDoc = userCollection.doc(userId);
+    CollectionReference moedasCollection = userDoc.collection('moedas');
+
+    // Limpe a coleção de moedas existentes antes de adicionar as novas moedas
+    await moedasCollection.get().then((querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        doc.reference.delete();
+      });
+    });
+
+    listaMoeda.forEach((moeda) async {
+      await moedasCollection.add(moeda.moedaJson());
+    });
   }
 }
